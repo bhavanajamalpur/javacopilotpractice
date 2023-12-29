0a93da4fdd506d484e57c5dce80d94175c03f113
bhavanajamalpur <154952154+bhavanajamalpur@users.noreply.github.com>
2023-12-29 15:05:56 +0530

    t1.py

---
+++ t1.py
@@ -0,0 +1,5 @@
+with open('data.txt', 'r') as file:
+    # Use Copilot to complete the code to read and print each line with line numbers
+    for line in file:
+        # TODO: Complete the code here
+      print(line)
